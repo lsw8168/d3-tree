e5cdf18720c8e93ea4d861f2599e2a4f99c31f32
lsw8168 <lsw8168@naver.com>
2018-04-12 15:06:38 +0900

    파일 추가, 센터정렬

--- tree2.js
+++ tree2.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the diagram
 var margin = {
         top: 20,
-        right: 180,
+        right: 200,
         bottom: 20,
         left: 20
     };
@@ -11,7 +11,7 @@ var clientHeight = document.getElementById("tree-container").clientHeight;
 var width = clientWidth - margin.left - margin.right;
 var maxLength;
 var height = 100 * maxLength;
-var rectWidth = 150;
+var rectWidth = 180;
 var rectHeight = 24;
 
 // declares a tree layout and assigns the size
@@ -65,10 +65,15 @@ d3.json("treeData.json", function(error, treeData) {
         .style("pointer-events", "all")
         .call(zoom)
 
-
     var g = svg.append("g")
         .attr("class","root")
         .attr("transform", "translate(" + margin.left + "," + -(height-clientHeight-100)/2  + ")");
+
+    console.log(height);
+    console.log(clientHeight);
+
+    var initScale = clientHeight / height;
+    console.log(initScale);
 
     zoomer.call(zoom.transform, d3.zoomIdentity.translate(margin.left,-(height-clientHeight-100)/2));
 
@@ -112,11 +117,9 @@ d3.json("treeData.json", function(error, treeData) {
         .attr("height", rectHeight)
         .attr("x", 0)
         .attr("y", -rectHeight / 2)
-        .text(function(d) {
+        .filter(function(d) {
             if(!d.data.name) {
                 return d3.select(this).remove();
-            } else {
-                return d.data.name;
             }
         });
 
@@ -131,20 +134,24 @@ d3.json("treeData.json", function(error, treeData) {
             }
         })
         .each(wrap)
-        // .on("click", function(d){
-        //     return ct2(d);
-        // });
+        .filter(function(d) {
+            return d.data.type == 'file' && d.data.file !== undefined;
+        })
+        .attr("class", "ip_link")
+        .on("click", function(d){
+            return click(d);
+        });
 
     node.append("rect")
         .attr("width", rectWidth)
         .attr("height", rectHeight)
         .attr("x", 0)
         .attr("y", -rectHeight / 2 + 24)
-        .text(function(d) {
-            if(!d.data.ip) {
+        .filter(function(d) {
+            if (d.data.type == 'file') {
+                return d.data.file !== undefined ? d.data.file : d3.select(this).remove();
+            } else if (d.data.type !== 'file' && !d.data.ip) {
                 return d3.select(this).remove();
-            } else {
-                return d.data.ip;
             }
         });
 
@@ -153,14 +160,16 @@ d3.json("treeData.json", function(error, treeData) {
         .attr("dy", 29)
         .attr("dx", 5)
         .text(function(d) {
-            if(!d.data.ip) {
-                return d3.select(this).remove();
-            } else {
+            if (d.data.type !== 'file') {
                 return d.data.ip;
+            } else {
+                return d.data.file !== undefined ? d.data.file.sample_risk_level == 2 ? "악성" : "정상" : undefined;
             }
         })
         .filter(function(d) {
-            return d.data.url !== '' ? d.data.url : '';
+            if (d.data.type !== 'file') {
+                return d.data.url !== '' ? d.data.url : '';
+            }
         })
         .attr("class", "ip_link")
         .on("click", function(d){
@@ -176,11 +185,11 @@ d3.json("treeData.json", function(error, treeData) {
         .attr("height", rectHeight)
         .attr("x", 0)
         .attr("y", -rectHeight / 2 + 48)
-        .text(function(d) {
-            if(!d.data.tag) {
+        .filter(function(d) {
+            if (d.data.type == 'file') {
+                return d.data.file !== undefined ? d.data.file : d3.select(this).remove();
+            } else if (d.data.type !== 'file' && !d.data.tag) {
                 return d3.select(this).remove();
-            } else {
-                return d.data.tag;
             }
         });
 
@@ -188,10 +197,10 @@ d3.json("treeData.json", function(error, treeData) {
         .attr("dy", 53)
         .attr("dx", 5)
         .text(function(d) {
-            if(!d.data.tag) {
-                return d3.select(this).remove();
-            } else {
+            if (d.data.type !== 'file') {
                 return d.data.tag;
+            } else {
+                return d.data.file !== undefined ? d.data.file.sample_tag : undefined;
             }
         })
         .each(wrap);
@@ -202,12 +211,17 @@ d3.json("treeData.json", function(error, treeData) {
         .enter()
         .append("path")
         .attr("class", function(d) {
-            return d.data.warning === true ? "link warning" : "link"
+            if (d.data.type !== "file") {
+                return d.data.warning === true ? "warning" : "";
+            } else {
+                return d.data.warning === true ? "file" : "";
+            }
+
         })
         .attr("d", function(d) {
             return "M" + d.y + "," + d.x + "C" + (
                 d.y + (d.parent.y + rectWidth)) / 2 + "," + d.x + " " + (
-                    d.y + (d.parent.y + rectWidth)) / 2 + "," + d.parent.x + " " + (d.parent.y + rectWidth) + "," + d.parent.x; //150은 rect width
+                    d.y + (d.parent.y + rectWidth)) / 2 + "," + d.parent.x + " " + (d.parent.y + rectWidth) + "," + d.parent.x; //180은 rect width
                 });
 
     var element = d3.select('.root').node();
